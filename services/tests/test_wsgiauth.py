392b2de91d734ed5cb25f888eb233aa34b37b2e1
Ryan Kelly <ryan@rfk.id.au>
2011-10-07 14:58:07 +1100

    Bug 692705 - refactor auth tests for easy subclass testing; r=tarek

--- services/tests/test_wsgiauth.py
+++ services/tests/test_wsgiauth.py
@@ -37,49 +37,241 @@
 # ***** END LICENSE BLOCK *****
 import unittest
 import base64
-from webob.exc import HTTPUnauthorized
+
+from webob import Response
+from webob.exc import HTTPUnauthorized, HTTPException
 
 from services.config import Config
 from services.wsgiauth import Authentication
 from services.auth.dummy import DummyAuth
+from services.user.memory import MemoryUser
 from services.tests.support import make_request
 
 
-class AuthTool(DummyAuth):
+class BadPasswordAuthTool(DummyAuth):
+    """Auth backend that refuses passwords containingg the letters b, a, d.
+
+    This Auth backend will accept any usernames and passwords except those
+    that contain the letters "b", "a", and "d" in any order.  It makes it easy
+    to test a variety of passwords without having to re-configure each time.
+
+    For successful auth, the user id is always 1.
+    """
 
     def authenticate_user(self, *args):
-        if args[0] == 'tarekbad':
-            return None
+        for value in args[:2]:
+            if "b" in value and "a" in value and "d" in value:
+                return None
         return 1
 
 
-class ColonPasswordAuthTool(DummyAuth):
+class BadPasswordUserTool(MemoryUser):
+    """User backend that refuses passwords containing the letters b, a, d.
 
-    def authenticate_user(self, *args):
-        if args[0] == 'user' and args[1] == 'pass:word:':
-            return 1
-        return None
+    This User backend will accept any usernames and passwords except those
+    that contain the letters "b", "a", and "d" in any order.  It makes it easy
+    to test a variety of passwords without having to re-configure each time.
+
+    For successful auth, the user id is always 1.
+    """
+
+    def authenticate_user(self, user, password, *args):
+        for value in (user["username"], password):
+            if "b" in value and "a" in value and "d" in value:
+                return None
+        return 1
 
 
-class AuthenticationTestCase(unittest.TestCase):
+class AuthAPITestCases(object):
+    """TestCases for the public Authentication API.
 
-    def test_password_colons(self):
-        # Passwords that contain colons are passed through to the
-        # authentication function.
-        config = Config({'auth.backend':
-                  'services.tests.test_wsgiauth.ColonPasswordAuthTool'})
-        auth = Authentication(config)
-        token = 'Basic ' + base64.b64encode('user:pass:word:')
+    This test case suite can be used to test the basic functionality of
+    any Authentication class.  They use only the public API i.e. the
+    "check" and "acknowledge" methods.
+
+    If the auth class does not understand HTTP-Basic-Auth credentials, you
+    can override the set_credentials() method to implement whatever scheme
+    it requires.
+
+    Note that this is *not* a unittest.TestCase suite; you will need to mix
+    it into an appropriate TestCase suite in order to use it.
+    """
+
+    auth_class = None
+
+    BASE_CONFIG = {'cef.version': '0.0',
+                   'cef.vendor': 'test',
+                   'cef.device_version': '0.0',
+                   'cef.product': 'test',
+                   'cef.file': 'test',
+                   'auth.backend':
+                        'services.tests.test_wsgiauth.BadPasswordAuthTool'}
+
+    def make_config(self, items=None):
+        config = self.BASE_CONFIG.copy()
+        if items:
+            config.update(items)
+        return Config(config)
+
+    def set_credentials(self, request, username, password):
+        """Set the given user credentials on the request.
+
+        This default implementation sets HTTP-Basic-Auth credentials.
+        Override it if your Authentication subclass needs something else.
+        """
+        password = password.encode("utf8")
+        token = 'Basic ' + base64.b64encode('%s:%s' % (username, password))
+        request.headers["Authorization"] = token
+
+    def setUp(self):
+        #  Check that we're not running from the base class.
+        assert self.auth_class is not None
+
+    def test_check_method(self):
+        config = self.make_config()
+        auth = self.auth_class(config)
+
+        #  check() should pass through requests where no auth is required
+        req = make_request('/1.0/tarek/info/collections')
+        auth.check(req, {})
+
+        #  check() should demand auth when required by the match.
+        req = make_request('/1.0/tarek/info/collections')
+        self.assertRaises(HTTPException, auth.check, req, {"auth": "True"})
+
+        #  check() should fail auth when the password is bad
+        req = make_request('/1.0/tarek/info/collections')
+        self.set_credentials(req, "user", "badpwd")
+        self.assertRaises(HTTPException, auth.check, req, {"auth": "True"})
+
+        #  check() should pass through when the password is good
+        req = make_request('/1.0/tarek/info/collections')
+        self.set_credentials(req, "user", "goodpwd")
+        auth.check(req, {"auth": "True"})
+
+        #  check() should fail auth if username doesn't match the match
+        match = {"auth": "True", "username": "user1"}
+        req = make_request('/1.0/tarek/info/collections')
+        self.set_credentials(req, "user2", "goodpwd")
+        self.assertRaises(HTTPException, auth.check, req, match)
+
+        #  check() should pass through if username matches the match
+        match = {"auth": "True", "username": "user1"}
+        req = make_request('/1.0/tarek/info/collections')
+        self.set_credentials(req, "user1", "goodpwd")
+        auth.check(req, match)
+
+    def test_acknowledge_method(self):
+        config = self.make_config()
+        auth = self.auth_class(config)
+
+        # Actually there's not much to test here.
+        # Just make sure it doesn't raise anything.
+        req = make_request('/1.0/tarek/info/collections')
+        resp = Response(status="200 OK", request=req)
+        auth.acknowledge(req, resp)
+
+        req = make_request('/1.0/tarek/info/collections')
+        self.set_credentials(req, "user", "goodpwd")
+        auth.check(req, {"auth": "True"})
+        resp = Response(status="200 OK", request=req)
+        auth.acknowledge(req, resp)
+
+    def test_unusual_passwords(self):
+        config = self.make_config()
+        auth = self.auth_class(config)
+
+        # Check that passwords containing unusual characters still work.
+        unusual_chars = u":!@= \t\n\N{GREEK SMALL LETTER ALPHA}\N{SNOWMAN}"
+        for char in unusual_chars:
+
+            # works at start of good pwd
+            req = make_request('/1.0/tarek/info/collections')
+            self.set_credentials(req, "user", char + "goodpwd")
+            auth.check(req, {"auth": "True"})
+
+            # works at end of good pwd
+            req = make_request('/1.0/tarek/info/collections')
+            self.set_credentials(req, "user", "goodpwd" + char)
+            auth.check(req, {"auth": "True"})
+
+            # works in middle of good pwd
+            req = make_request('/1.0/tarek/info/collections')
+            self.set_credentials(req, "user", "good" + char + "pwd")
+            auth.check(req, {"auth": "True"})
+
+            # fails at start of bad pwd
+            req = make_request('/1.0/tarek/info/collections')
+            self.set_credentials(req, "user", char + "badpwd")
+            self.assertRaises(HTTPException, auth.check, req, {"auth": "True"})
+
+            # fails at end of bad pwd
+            req = make_request('/1.0/tarek/info/collections')
+            self.set_credentials(req, "user", "badwd" + char)
+            self.assertRaises(HTTPException, auth.check, req, {"auth": "True"})
+
+            # fails in middle of bad pwd
+            req = make_request('/1.0/tarek/info/collections')
+            self.set_credentials(req, "user", "bad" + char + "pwd")
+            self.assertRaises(HTTPException, auth.check, req, {"auth": "True"})
+
+
+class HTTPBasicAuthAPITestCases(AuthAPITestCases):
+    """TestCases for the public Authentication API using HTTP-Basic-Auth.
+
+    This test case suite can be used to test the functionality of any
+    Authentication class that interprets HTTP-Basic-Auth headers. It uses
+    only the public API i.e. the "check" and "acknowledge" methods.
+
+    Note that this is *not* a unittest.TestCase suite; you will need to mix
+    it into an appropriate TestCase suite in order to use it.
+    """
+
+    def test_bad_utf8_password(self):
+        config = self.make_config()
+        auth = self.auth_class(config)
+
+        password = u'И'.encode('cp866')
+        token = 'tarek:%s' % password
+        token = 'Basic ' + base64.b64encode(token)
         req = make_request('/1.0/tarek/info/collections',
-                            {'HTTP_AUTHORIZATION': token})
-        res = auth.authenticate_user(req, {})
-        self.assertEquals(res, 1)
+                           {'HTTP_AUTHORIZATION': token})
+        self.assertRaises(HTTPUnauthorized, auth.check, req, {"auth": "True"})
+
+    def test_malformed_auth_headers(self):
+        config = self.make_config()
+        auth = Authentication(config)
+
+        req = make_request('/1.0/tarekbad',
+                            {'HTTP_AUTHORIZATION': 'Basic ',
+                             'REQUEST_METHOD': 'TEST',
+                             'PATH_INFO': 'TEST'})
+        self.assertRaises(HTTPUnauthorized, auth.check, req, {"auth": "True"})
+
+        req = make_request('/1.0/tarekbad',
+                            {'HTTP_AUTHORIZATION': 'Basic invalid_b64',
+                             'REQUEST_METHOD': 'TEST',
+                             'PATH_INFO': 'TEST'})
+        self.assertRaises(HTTPUnauthorized, auth.check, req, {"auth": "True"})
+
+        req = make_request('/1.0/tarekbad',
+                            {'HTTP_AUTHORIZATION': 'Basic ' +
+                              base64.b64encode('malformed_creds'),
+                             'REQUEST_METHOD': 'TEST',
+                             'PATH_INFO': 'TEST'})
+        self.assertRaises(HTTPUnauthorized, auth.check, req, {"auth": "True"})
+
+
+class TestAuthentication(HTTPBasicAuthAPITestCases, unittest.TestCase):
+    """Tests for base Authentication class."""
+
+    auth_class = Authentication
 
     def test_authenticate_user(self):
 
-        config = Config({'auth.backend':
-                         'services.tests.test_wsgiauth.AuthTool'})
-        auth = Authentication(config)
+        config = self.make_config()
+        auth = self.auth_class(config)
+
         token = 'Basic ' + base64.b64encode('tarek:tarek')
         req = make_request('/1.0/tarek/info/collections', {})
         res = auth.authenticate_user(req, {})
@@ -96,38 +288,37 @@ class AuthenticationTestCase(unittest.TestCase):
         bad_token2 = 'Basic' + base64.b64encode('tarek:tarek')
         req = make_request('/1.0/tarek/info/collections',
                            {'HTTP_AUTHORIZATION': bad_token1})
-
         self.assertRaises(HTTPUnauthorized, auth.authenticate_user, req,
-                          {})
+                          config)
+
         req = make_request('/1.0/tarek/info/collections',
                            {'HTTP_AUTHORIZATION': bad_token2})
         self.assertRaises(HTTPUnauthorized, auth.authenticate_user, req,
-                          {})
+                          config)
+
         # check a bad request to an invalid user.
         req = make_request('/1.0/tarekbad',
                            {'HTTP_AUTHORIZATION': 'Basic ' +
                             base64.b64encode('tarekbad:tarek'),
                             'REQUEST_METHOD': 'TEST',
                             'PATH_INFO': 'TEST'})
-        # the following options are required for cef dependency
         self.assertRaises(HTTPUnauthorized, auth.authenticate_user, req,
-                          {'cef.version': '0.0',
-                           'cef.vendor': 'test',
-                           'cef.device_version': '0.0',
-                           'cef.product': 'test',
-                           'cef.file': 'test',
-                            }, 'tarekbad')
+                          config)
 
-    def test_bad_password(self):
-        config = Config({'auth.backend':
-                         'services.tests.test_wsgiauth.AuthTool'})
-        auth = Authentication(config)
 
-        password = u'И'.encode('cp866')
-        token = 'tarek:%s' % password
-        token = 'Basic ' + base64.b64encode(token)
-        req = make_request('/1.0/tarek/info/collections',
-                           {'HTTP_AUTHORIZATION': token})
+class TestAuthentication_NewStyleAuth(TestAuthentication):
 
-        self.assertRaises(HTTPUnauthorized, auth.authenticate_user, req,
-                          {})
+    BASE_CONFIG = TestAuthentication.BASE_CONFIG.copy()
+    BASE_CONFIG["auth.backend"] = \
+                        'services.tests.test_wsgiauth.BadPasswordUserTool'
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestAuthentication))
+    suite.addTest(unittest.makeSuite(TestAuthentication_NewStyleAuth))
+    return suite
+
+
+if __name__ == "__main__":
+    unittest.main(defaultTest="test_suite")
